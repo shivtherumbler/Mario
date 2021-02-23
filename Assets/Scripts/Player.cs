60b2e534c7492d072d4538987b13816466b420cd
shivtherumbler <shivtherumbler@users.noreply.github.com>
2021-02-23 10:55:43 +0530

    Add Player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        //var v = Input.GetAxis("Vertical");
+
+        rigidBody2D.velocity = new Vector2(h * speed, rigidBody2D.velocity.y);
+
+        if(h<0)
+        {
+            spriteRenderer.flipX = true;
+            //transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
+        }
+        else if(h>0)
+        {
+            spriteRenderer.flipX = false;
+            //transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }    
+        
+    }
+
+}
